e00653b586a7bbeb57ccd7f8e66e1c9f104c787a
apercelay-max <apercelay@gmail.com>
2026-07-25 10:35:37 +0200

    Ajout icone IconShare

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -304,3 +304,13 @@ export const IconUpload: React.FC<IconProps> = ({ size = 18, color = 'currentCol
     <line x1="12" y1="3" x2="12" y2="15" />
   </svg>
 );
+
+export const IconShare: React.FC<IconProps> = ({ size = 18, color = 'currentColor' }) => (
+  <svg width={size} height={size} viewBox='0 0 24 24' {...base} stroke={color}>
+    <circle cx='18' cy='5' r='3' />
+    <circle cx='6' cy='12' r='3' />
+    <circle cx='18' cy='19' r='3' />
+    <line x1='8.59' y1='13.51' x2='15.42' y2='17.49' />
+    <line x1='15.41' y1='6.51' x2='8.59' y2='10.49' />
+  </svg>
+);
